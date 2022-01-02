602b0f2d2f7cbc19df61a2a4c32dbc6a824d3967
onsah <sahinonur2000@hotmail.com>
2022-01-02 14:32:34 +0300

    Separate terminal interface from compilation logic

--- src/TigerC.ml
+++ src/TigerC.ml
@@ -1,23 +1,24 @@
 open Core
 
+let compile path =
+  Result.map (Parser.parse_file ~path) ~f:(fun expr ->
+      ignore (FindEscape.find_escape expr) ;
+      ignore (Printf.printf "%s\n" (Syntax.show_expr expr)) ;
+      Semant.type_check expr )
+
+
 let () =
   let args = Array.to_list (Sys.get_argv ()) in
   match args with
   | _ :: path :: _ ->
-      let expr =
-        match Parser.parse_file ~path with
-        | Ok expr ->
-            expr
-        | Error err ->
-            eprintf
-              "Error : %s\n\n%s\n"
-              (Exn.to_string err)
-              (Printexc.get_backtrace ()) ;
-            exit 1
-      in
-      ignore (FindEscape.find_escape expr) ;
-      ignore (Printf.printf "%s\n" (Syntax.show_expr expr)) ;
-      let typed_expr = Semant.type_check expr in
-      Printf.printf "%s\n" (Translated.show_typedExpr typed_expr)
+    ( match compile path with
+    | Ok typed_expr ->
+        Printf.printf "%s\n" (Translated.show_typedExpr typed_expr)
+    | Error err ->
+        eprintf
+          "Error : %s\n\n%s\n"
+          (Exn.to_string err)
+          (Printexc.get_backtrace ()) ;
+        exit 1 )
   | _ ->
       Printf.printf "[TigerC] Usage: tigerC <path>\n"
