54c2752890f0b1f7e42eb70b59cde08cc441aaf5
onsah <sahinonur2000@hotmail.com>
2022-01-02 16:18:22 +0300

    test conditional and if

--- test/semant/translate/TranslateE2ETest.ml
+++ test/semant/translate/TranslateE2ETest.ml
@@ -2,7 +2,6 @@ open OUnit2
 open Ir
 
 let test_regular_if _ =
-  ignore (Core.printf "path %s\n" (Sys.getcwd ())) ;
   let expr =
     Result.get_ok
       (Main.analyze_file ~path:"../../scripts/translate/regular_if.tig")
@@ -36,9 +35,52 @@ let test_regular_if _ =
           false )
 
 
+let test_conitional_and_if _ =
+  let expr =
+    Result.get_ok
+      (Main.analyze_file ~path:"../../scripts/translate/conditional_and_if.tig")
+  in
+  ignore (assert_equal expr.ty Types.Int) ;
+  ignore
+    ( match expr.translated_expr.translated_expr with
+    | Expr
+        (ESeq
+          ( Seq
+              [ IRTree.CondJump
+                  { true_label = true_label_1'; false_label = false_label'; _ }
+              ; IRTree.Label true_label_1
+              ; IRTree.CondJump
+                  { true_label = true_label_2'; false_label = false_label''; _ }
+              ; IRTree.Label true_label_2
+              ; IRTree.Move { location = IRTree.Temp result'; value = Const 1 }
+              ; IRTree.Jump { labels = [ join_label' ]; _ }
+              ; IRTree.Label false_label
+              ; IRTree.Move { location = IRTree.Temp result''; value = Const 0 }
+              ; IRTree.Label join_label
+              ]
+          , IRTree.Temp result ) )
+      when true_label_1' = true_label_1
+           && false_label' = false_label
+           && true_label_2' = true_label_2
+           && false_label'' = false_label
+           && join_label' = join_label
+           && result' = result
+           && result'' = result ->
+        assert_bool "matched pattern" true
+    | _ ->
+        assert_bool
+          (Core.sprintf
+             "didn't match pattern: %s"
+             (Translated.show_typedExpr expr) )
+          false )
+
+
 let suite =
   "Translate"
-  >::: [ "if_else should process if expressions normally" >:: test_regular_if ]
+  >::: [ "if_else should process if expressions normally" >:: test_regular_if
+       ; "if_else should optimize if expressions with comparions"
+         >:: test_conitional_and_if
+       ]
 
 
 let _ = run_test_tt_main suite
